f962bc4561947e2589aa58161741cd27dfa9ae06
istrastr2020 <154003033+istrastr2020@users.noreply.github.com>
2023-12-27 18:03:06 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1703681038,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/фон-sheet0.png",
+		"images/конь-sheet0.png",
+		"images/камень-sheet0.png",
+		"images/энергия-sheet0.png",
+		"images/дверь-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт6-sheet0.png",
+		"images/фейковыйстул-sheet0.png",
+		"images/фейковыйдазай-sheet0.png",
+		"images/Спрайт7-sheet0.png",
+		"images/Спрайт11-sheet0.png",
+		"images/Спрайт12-sheet0.png",
+		"images/Спрайт14-sheet0.png",
+		"images/Спрайт15-sheet0.png",
+		"images/Спрайт16-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
